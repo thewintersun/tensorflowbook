adf0ae4cf0803dd63c4504fd3c5e0bc4304686ee
thewintersun <65361006@qq.com>
2017-02-13 01:15:27 +0800

    add v2

---
+++ Chapter3/3.6/3.6.2/visual_learning2.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+import numpy as np
+import tensorflow as tf
+
+# 构建图
+x = tf.placeholder(tf.float32, shape=[3,1])
+y = tf.placeholder(tf.float32, shape=[2])
+weight = tf.get_variable("weight", [3,2], tf.float32, initializer=tf.random_normal_initializer())
+biase  = tf.get_variable("biase", [2], tf.float32, initializer=tf.random_normal_initializer())
+x_trans = tf.transpose(x)
+mul_op = tf.matmul(x_trans, weight, name="mul_op")
+pred = tf.add(mul_op, biase, name="add_op")
+
+#损失函数
+loss = tf.square(y - pred, name="loss")
+#优化函数
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+#计算梯度，应用梯度操作
+grads_and_vars = optimizer.compute_gradients(loss)
+train_op = optimizer.apply_gradients(grads_and_vars)
+
+#收集值的操作
+#tf.summary.scalar("weight", weight)
+#tf.summary.scalar("biase", biase)
+#tf.summary.scalar("loss", loss[0])
+
+merged_summary = tf.summary.merge_all()
+
+summary_writer = tf.summary.FileWriter('./log_graph' )
+summary_writer.add_graph(tf.get_default_graph())
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init_op)
+    for step in range(500):
+        print(step)
+        train_x = [np.random.randn(1),np.random.randn(1),np.random.randn(1)]
+        #y1 = train_x[0] * 1 + train_x[1] * 2 + train_x[2] * 3 + 10 + np.random.randn(1) * 0.01
+        #y2 = train_x[0] * 4 + train_x[1] * 5 + train_x[2] * 6 + 20 + np.random.randn(1) * 0.01
+
+        '''
+        train_x = [[np.random.randn(1),np.random.randn(1),np.random.randn(1)]]
+        y1 = train_x[0][1] * 1 + train_x[0][1] * 2 + train_x[0][2] * 3 + 10 + np.random.randn(1) * 0.01
+        y2 = train_x[0][1] * 4 + train_x[0][1] * 5 + train_x[0][2] * 6 + 20 + np.random.randn(1) * 0.01
+        '''
+        train_y = [1, 2]
+        #_, summary = sess.run([train_op, merged_summary], feed_dict={x:train_x, y:train_y})
+        _ = sess.run([train_op], feed_dict={x:train_x, y:train_y})
+        summary_writer.add_summary(summary, step)
+        
