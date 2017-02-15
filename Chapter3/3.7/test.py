3666c3908e02bd9cb840017d9471ac737cd34012
thewintersun <65361006@qq.com>
2017-02-16 00:25:45 +0800

    add file

---
+++ Chapter3/3.7/test.py
@@ -0,0 +1,7 @@
+import math
+
+print ("sin(3) : %f"%math.sin(3))
+print ("sin(-3) : %f"%math.sin(-3))
+print ("sin(0) : %f"%math.sin(0))
+print ("sin(math.pi) %f: "%  math.sin(math.pi))
+print ("sin(math.pi/2) %f: "%  math.sin(math.pi/2))
