6a74899c7c19c3bd202791aff3738bb059fd008e
luodongri <luodongri@gmail.com>
2017-04-19 22:10:33 +0800

    add sin function

--- Chapter4/4.7/sin_example.py
+++ Chapter4/4.7/sin_example.py
@@ -3,86 +3,93 @@
 import tensorflow as tf
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import types
+import pylab
 
 '''
 用tensorflow来拟合一个正弦函数
 
 '''
 
+def draw_correct_line():
+  '''
+  绘制标准的sin的曲线
+  '''
+  x = np.arange(0, 2 * np.pi, 0.01)
+  x = x.reshape((len(x), 1))
+  y = np.sin(x)
+
+  pylab.plot(x,y, label = '标准sin曲线')
+  plt.axhline(linewidth=1, color='r')
+
+  
+
 def get_train_data():
   '''返回一个训练样本(train_x, train_y), 
     其中train_x是随机的自变量， train_y是train_x的sin函数值
   '''
-  train_x = np.random.uniform(0.0, 4.0, (1))
-  train_y = math.sin(train_x)
+  train_x = np.random.uniform(0.0, 2 * np.pi, (1))
+  train_y = np.sin(train_x)
   return train_x, train_y
 
 
-def get_test_data():
-  '''返回一个测试数据(test_x, test_y), 
-    
-  '''
-  test_x = np.random.uniform(0.0, 4.0, (1))
-  test_y = math.sin(test_x)
-  return test_x, test_y
-  
-def test(input_data):
-  with tf.variable_scope('test'):
-    weights = tf.get_variable("weight", [1, 16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [1, 16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    hidden1 = tf.sigmoid(tf.multiply(input_data, weights) + biases)
-  with tf.variable_scope('test2'):
-    weights = tf.get_variable("weight", [16, 16],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    mul = tf.matmul(hidden1, weights)
-    hidden2 = tf.sigmoid(mul + biases)
-  with tf.variable_scope('test3'):
-    weights = tf.get_variable("weight", [16, 16],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    hidden3 = tf.sigmoid(tf.matmul(hidden2, weights) + biases)
-
-  with tf.variable_scope('testoutput_layer'):
-    weights = tf.get_variable("weight", [16, 1],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [1], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    output = tf.sigmoid(tf.matmul(hidden3, weights) + biases)
-  return tf.shape(hidden1), tf.shape(weights), output
-
 def inference(input_data):
+  '''
+  定义前向计算的网络结构
+  Args:
+     输入的x的值，单个值
+  '''
   with tf.variable_scope('hidden1'):
-    weights = tf.get_variable("weight", [1, 16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [1, 16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
+    #第一个隐藏层，采用16个隐藏节点
+    weights = tf.get_variable("weight", [1, 16], tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
+    biases  = tf.get_variable("biase", [1, 16], tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
     hidden1 = tf.sigmoid(tf.multiply(input_data, weights) + biases)
-
 
   with tf.variable_scope('hidden2'):
-    weights = tf.get_variable("weight", [16, 16],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
+    #第二个隐藏层，采用16个隐藏节点
+    weights = tf.get_variable("weight", [16, 16],  tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
+    biases  = tf.get_variable("biase", [16], tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
     mul = tf.matmul(hidden1, weights)
     hidden2 = tf.sigmoid(mul + biases)
 
   with tf.variable_scope('hidden3'):
-    weights = tf.get_variable("weight", [16, 16],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [16], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
+    #第三个隐藏层，采用16个隐藏节点
+    weights = tf.get_variable("weight", [16, 16],  tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
+    biases  = tf.get_variable("biase", [16], tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
     hidden3 = tf.sigmoid(tf.matmul(hidden2, weights) + biases)
 
   with tf.variable_scope('output_layer'):
-    weights = tf.get_variable("weight", [16, 1],  tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    biases  = tf.get_variable("biase", [1], tf.float32, initializer=tf.random_normal_initializer(0.0, 0.1))
-    output = tf.sigmoid(tf.matmul(hidden3, weights) + biases)
+    #输出层
+    weights = tf.get_variable("weight", [16, 1],  tf.float32, 
+                    initializer=tf.random_normal_initializer(0.0, 1))
+    biases  = tf.get_variable("biase", [1], tf.float32,  
+                    initializer=tf.random_normal_initializer(0.0, 1))
+    output = tf.matmul(hidden3, weights) + biases
 
   return output
 
 
 def train():
-  learning_rate = 0.0001
+  #学习率
+  learning_rate = 0.01
+  
   x = tf.placeholder(tf.float32)
   y = tf.placeholder(tf.float32)
   
-  
   net_out = inference(x)
+  
+  #定义损失函数的op
   loss = tf.square(net_out - y)
+  
+  #采用随机梯度下降的优化函数
   opt = tf.train.GradientDescentOptimizer(learning_rate)
-  #opt = tf.train.AdamOptimizer(learning_rate)
   train_op = opt.minimize(loss)
   
   init = tf.global_variables_initializer()
@@ -93,23 +100,22 @@ def train():
     for i in range(1000000):
       train_x, train_y = get_train_data()
       sess.run(train_op, feed_dict={x: train_x, y: train_y})
-      '''
-      t, t2, m = sess.run([tt, tt2, mm], feed_dict={x: train_x, y: train_y})
-      print(t)
-      print(t2)
-      print(m)
-	  '''
 
-      if i % 1000 == 0:
-        test_x, test_y = get_test_data()
-        loss_value = sess.run(loss, feed_dict={x: test_x, y: test_y})
-        print("step %d test loss is %f" %(i , loss_value))
-
-
-
-
-
+      if i % 10000 == 0:
+        times = int(i / 10000)
+        test_x_ndarray = np.arange(0, 2 * np.pi, 0.01)
+        test_y_ndarray = np.zeros([len(test_x_ndarray)])
+        ind = 0
+        for test_x in test_x_ndarray:
+          test_y = sess.run(net_out, feed_dict={x: test_x, y: 1})
+          np.put(test_y_ndarray, ind, test_y)
+          ind += 1
+        # 先绘制标准的sin函数的曲线，
+        # 再用虚线绘制我们计算出来模拟sin函数的曲线
+        draw_correct_line()
+        pylab.plot(test_x_ndarray,test_y_ndarray,'--', label =  str(times) + 'times' )
+        pylab.show()
 
 if __name__ == "__main__":
-  pass
   train()
+
