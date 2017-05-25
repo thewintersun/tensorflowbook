798ddcce9ae7e136fdb0f172e08019ffeaa98988
luodongri <luodongri@gmail.com>
2017-05-25 22:00:40 +0800

    add display

--- Chapter5/5.5/display.py
+++ Chapter5/5.5/display.py
@@ -7,9 +7,8 @@ import os
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_float('gpu_memory_fraction', 0.02, 'gpu占用内存比例')
 tf.app.flags.DEFINE_integer('batch_size', 10, 'batch_size大小')
-tf.app.flags.DEFINE_integer('reload_model', 0, '是否reload之前训练好的模型')
 tf.app.flags.DEFINE_string('model_dir', "./model/", '保存模型的文件夹')
-tf.app.flags.DEFINE_string('event_dir', "./event/", '保存event数据的文件夹,给tensorboard展示用')
+tf.app.flags.DEFINE_string('event_dir', "./event/", '保存模型的文件夹')
 
 def weight_init(shape, name):
     '''
@@ -44,8 +43,7 @@ def read_and_decode(filename_queue):
   image = tf.decode_raw(features['image_data'], tf.uint8)
   image = tf.reshape(image, [100, 100, 3])
 
-  image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-  #image = tf.cast(image, tf.float32) 
+  image = tf.cast(image, tf.float32)
 
   label = tf.cast(features['label'], tf.int32)
 
@@ -59,10 +57,9 @@ def inputs(filename, batch_size):
 
     image, label = read_and_decode(filename_queue)
 
-    images, labels = tf.train.shuffle_batch(
+    images, labels = tf.train.batch(
         [image, label], batch_size=batch_size, num_threads=1,
-        capacity=4,
-        min_after_dequeue=2)
+        capacity=4)
 
     return images, labels
 
@@ -112,98 +109,72 @@ def inference(input_data):
 
     h_fc2 = tf.matmul(h_fc, w_fc2) + b_fc2
 
-  return h_fc2
+  return h_fc2, h_conv1, h_conv2, h_conv3, h_pool3, h_fc
 
 
 def train():
   '''
   训练过程
   '''
-  global_step = tf.get_variable('global_step', [],
-                                initializer=tf.constant_initializer(0),
-                                trainable=False, dtype=tf.int32)
-
   batch_size = FLAGS.batch_size
-  train_images, train_labels = inputs("./tfrecord_data/train.tfrecord", batch_size )
-  test_images, test_labels = inputs("./tfrecord_data/train.tfrecord", batch_size )
-  
-  
-  train_labels_one_hot = tf.one_hot(train_labels, 2, on_value=1.0, off_value=0.0)
-  test_labels_one_hot = tf.one_hot(test_labels, 2, on_value=1.0, off_value=0.0)
-
-  #因为任务比较简单，故意把学习率调小了，以拉长训练过程。
-  learning_rate = 0.0000001
-  
+  train_images, train_labels = inputs("./tfrecord_data/display.tfrecord", batch_size )
   
   with tf.variable_scope("inference") as scope:
-    train_y_conv = inference(train_images)
-    scope.reuse_variables()
-    test_y_conv = inference(test_images)
+    train_y_conv,  h_conv1, h_conv2, h_conv3, h_pool3, h_fc1 = inference(train_images)
+    
+  for i in range(16):
+    image_channel = tf.slice(h_conv1, [0,0,0,i], [10, 91, 91,1])
+    tf.summary.image("conv1_image", image_channel, 2)
+  for i in range(16):
+    image_channel = tf.slice(h_conv2, [0,0,0,i], [10, 41, 41,1])
+    tf.summary.image("conv2_image", image_channel, 2) 
+  for i in range(16):
+    image_channel = tf.slice(h_conv3, [0,0,0,i], [10, 16, 16,1])
+    tf.summary.image("conv3_image", image_channel, 2) 
+  for i in range(16):
+    image_channel = tf.slice(h_pool3, [0,0,0,i], [10, 8, 8,1])
+    tf.summary.image("pool3_image", image_channel, 2) 
+  summary_op = tf.summary.merge_all()
 
-  cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=train_labels_one_hot, logits=train_y_conv))
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-  train_op = optimizer.minimize(cross_entropy, global_step=global_step)
 
-  train_correct_prediction = tf.equal(tf.argmax(train_y_conv, 1), tf.argmax(train_labels_one_hot, 1))
-  train_accuracy = tf.reduce_mean(tf.cast(train_correct_prediction, tf.float32))
-
-  test_correct_prediction = tf.equal(tf.argmax(test_y_conv, 1), tf.argmax(test_labels_one_hot, 1))
-  test_accuracy = tf.reduce_mean(tf.cast(test_correct_prediction, tf.float32))
-  
-  
   init_op = tf.global_variables_initializer()
   local_init_op = tf.local_variables_initializer()
   
   saver = tf.train.Saver()
-  tf.summary.scalar('cross_entropy_loss', cross_entropy)
-  tf.summary.scalar('train_acc', train_accuracy)
-  summary_op = tf.summary.merge_all()
  
   gpu_options = tf.GPUOptions(
     per_process_gpu_memory_fraction=FLAGS.gpu_memory_fraction)
   config = tf.ConfigProto(gpu_options=gpu_options)
  
   with tf.Session(config=config) as sess:
-    if FLAGS.reload_model == 1:
-      ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
-      saver.restore(sess, ckpt.model_checkpoint_path)
-      save_step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
-      print("reload model from %s, save_step = %d" % (ckpt.model_checkpoint_path, save_step))
-    else:
-      print("Create model with fresh paramters.")
-      sess.run(init_op)
-      sess.run(local_init_op)
+    if not os.path.exists(FLAGS.model_dir):
+      print("model dir not exists. %s" % FLAGS.model_dir)
+      return 
+
+    ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
+    saver.restore(sess, ckpt.model_checkpoint_path)
+    save_step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
+    print("reload model from %s, save_step = %d" % (ckpt.model_checkpoint_path, save_step))
+
+    
+    sess.run(local_init_op)
 
     summary_writer = tf.summary.FileWriter(FLAGS.event_dir, sess.graph)
-
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    try:
-      while not coord.should_stop():
-        _, g_step = sess.run([train_op, global_step])
-        if g_step % 2 == 0:
-          summary_str = sess.run(summary_op)
-          summary_writer.add_summary(summary_str, g_step)
+    
+    for i in range(20):
+      h_fc1_value, summary_str = sess.run([train_y_conv, summary_op])
+      summary_writer.add_summary(summary_str, i)
+      print(h_fc1_value)
 
-        if g_step % 100 == 0:
-          train_accuracy_value, loss = sess.run([train_accuracy, cross_entropy])
-          print("step %d training_acc is %.2f, loss is %.4f" % (g_step, train_accuracy_value, loss))
-        if g_step % 1000 == 0:
-          test_accuracy_value = sess.run(test_accuracy)
-          print("step %d test_acc is %.2f" % (g_step, test_accuracy_value))
+    summary_writer.close()
+    coord.request_stop()
 
-        if g_step % 2000 == 0:
-          #保存一次模型
-          print("save model to %s" % FLAGS.model_dir  + "model.ckpt." + str(g_step) )
-          saver.save(sess, FLAGS.model_dir + "model.ckpt", global_step=global_step)
+    coord.join(threads)
 
-    except tf.errors.OutOfRangeError:
-      pass
-    finally:
-      coord.request_stop()
-
-    coord.join(threads) 
+    #conv1_data = sess.run([tf.shape(h_conv1)])
+    #print(conv1_data)
 
   
 if __name__ == "__main__":
@@ -212,7 +183,4 @@ if __name__ == "__main__":
     gen_tfrecord_data("./data/train", "./data/test/")
  
   os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
-  #test()
-  if not os.path.exists(FLAGS.model_dir):
-    os.makedirs(FLAGS.model_dir)
   train()
--- Chapter5/5.5/gen_tfrecord.py
+++ Chapter5/5.5/gen_tfrecord.py
@@ -56,6 +56,33 @@ def gen_tfrecord(zero_dir, one_dir, output_tfrecord_file):
 
   tf_writer.close()
 
+
+def gen_display_tfrecord(data_dir, output_tfrecord_file):
+  tf_writer = tf.python_io.TFRecordWriter(output_tfrecord_file)
+
+  #为数字0的数据
+  for file in os.listdir(data_dir):
+    file_path = os.path.join(data_dir, file)
+    image_data = cv2.imread(file_path)
+    image_bytes = image_data.tostring()
+    rows = image_data.shape[0]
+    cols = image_data.shape[1]
+    channels = image_data.shape[2]
+    label_data = 0
+
+    example = tf.train.Example()
+
+    feature = example.features.feature
+    feature['height'].int64_list.value.append(rows)
+    feature['width'].int64_list.value.append(cols)
+    feature['channels'].int64_list.value.append(channels)
+    feature['image_data'].bytes_list.value.append(image_bytes)
+    feature['label'].int64_list.value.append(label_data)
+
+    tf_writer.write(example.SerializeToString())
+  tf_writer.close()
+  
+
     
 def gen_tfrecord_data(train_data_dir, test_data_dir):
   '''
@@ -77,23 +104,11 @@ def gen_tfrecord_data(train_data_dir, test_data_dir):
   gen_tfrecord(train_data_zero_dir, train_data_one_dir, train_tfrecord_file)
   gen_tfrecord(test_data_zero_dir, test_data_one_dir, test_tfrecord_file)
 
-
-
-def test():
-  pass
-  images, labels = inputs("./tfrecord_data/train.tfrecord", 1 )
-  with tf.Session() as sess:
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    try:
-      while not coord.should_stop():
-        i = sess.run(images)
-        print(i)
-    except tf.errors.OutOfRangeError:
-      pass
-    finally:
-      coord.request_stop()
   
 if __name__ == "__main__":
   gen_tfrecord_data("./data/train", "./data/test/")
   print("generate tfrecord data complete!")
+
+  #生成展示卷积网络中间过程图形化的测试数据
+  display_test_pic_dir = "./data/display"
+  gen_display_tfrecord(display_test_pic_dir,  "./tfrecord_data/display.tfrecord")
