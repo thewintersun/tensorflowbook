5ec580cd7bf9133621b41db8b8b5eb2faedb21fe
thewintersun <65361006@qq.com>
2017-05-27 00:25:25 +0800

    home

--- Chapter5/5.5/gen_tfrecord.py
+++ Chapter5/5.5/gen_tfrecord.py
@@ -5,10 +5,25 @@ import os
 import cv2
 
 
+'''
+本文件由图片生成tfrecord文件，保存的信息有图片的像素信息，图片的高和宽，图片的通道数，
+图片对应的label，也就是图片是1还是0.
 
+会生成训练的tfrecord文件和验证的tfrecord文件，
+还会生成一个用来展示卷积过程中图片经过卷积变化的图片的tfrecord文件。
+'''
 
 def gen_tfrecord(zero_dir, one_dir, output_tfrecord_file):
   '''
+  读取文件夹中的图片数据， 生成tfrecord格式的文件
+  
+  Args:
+    zero_dir: 保存图片0的文件夹
+	one_dir: 保存图片1的文件夹
+	output_tfrecord_file: 输出的tfrecord文件
+	
+  Return:
+  
   '''
   tf_writer = tf.python_io.TFRecordWriter(output_tfrecord_file)
 
@@ -58,6 +73,19 @@ def gen_tfrecord(zero_dir, one_dir, output_tfrecord_file):
 
 
 def gen_display_tfrecord(data_dir, output_tfrecord_file):
+  '''
+  读取文件夹中的图片数据， 生成tfrecord格式的文件.
+  这个函数用来生成单独tfrecord文件，
+  在演示卷积过程的时候单独使用
+  
+  
+  Args:
+    data_dir: 保存图片的文件夹
+	output_tfrecord_file: 输出的tfrecord文件
+	
+  Return:
+  
+  '''
   tf_writer = tf.python_io.TFRecordWriter(output_tfrecord_file)
 
   #为数字0的数据
@@ -87,6 +115,12 @@ def gen_display_tfrecord(data_dir, output_tfrecord_file):
 def gen_tfrecord_data(train_data_dir, test_data_dir):
   '''
   生成训练和测试的tfrecord格式的数据
+  
+  Args:
+    train_data_dir: 训练图片数据的文件夹
+	test_data_dir:  测试图片数据的文件夹
+	
+  Return:
   '''
   train_data_zero_dir = os.path.join(train_data_dir, "0")  
   train_data_one_dir = os.path.join(train_data_dir, "1")  
@@ -107,8 +141,9 @@ def gen_tfrecord_data(train_data_dir, test_data_dir):
   
 if __name__ == "__main__":
   gen_tfrecord_data("./data/train", "./data/test/")
-  print("generate tfrecord data complete!")
+  
 
   #生成展示卷积网络中间过程图形化的测试数据
   display_test_pic_dir = "./data/display"
   gen_display_tfrecord(display_test_pic_dir,  "./tfrecord_data/display.tfrecord")
+  print("generate tfrecord data complete!")
